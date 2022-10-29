d98f352efda4025c06fde384559477ae09de1788
Marysia <borkowska.maria07@gmail.com>
2022-10-29 12:47:34 +0200

    change

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello!\n");
-    return 0;
+int main()
+    {
+      int a, b;
+      int pom;
+      printf("Podaj a=");
+      scanf("%d",&a);
+      printf("Podaj b=");
+      scanf("%d",&b);
+      pom=a;
+      a=b;
+      b=pom;
+      printf("a=%d",a);
+      printf("\nb=%d\n",b);
+        return 0;
 }
